46fd1b1ad8a2fbca3a3f25b1444ea3e593d728b0
pavitharani-ai <pavi.ai@outlook.com>
2026-04-02 00:56:53 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,16 @@
+#Day 1: The ECE-AI Bridge 
+voltage =230
+resistance =15
+
+
+#Engineering Logic: Ohm's Law 
+current =voltage / resistance
+
+print("---AI ENGINEER REPORT: DAY 1 ---")
+print(f"Current Flow: {current:.2f} Amps")
+
+#AI Decision Logic (If-Else)
+if current > 10:
+   print("AI Analysis: High Current detectec")
+else:
+   print("AI Analysis: Normal operating leve")
